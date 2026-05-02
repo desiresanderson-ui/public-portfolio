79e62eb6e188fc9bac68fa5621b6917bbdbf637b
desiresanderson-ui <desire.sanderson@gmail.com>
2026-05-02 09:50:17 -0400

    Refactor HTML structure in module13.php
    Removed unnecessary PHP code and fixed HTML structure.

--- module13/module13.php
+++ module13/module13.php
@@ -7,21 +7,14 @@
     <title>Module13</title>
 </head>
 <body>
-    
-</body>
 
-       <h1>Module 13</h1>
+    <h1>Module 13</h1>
+
     <form method="POST" action="">
         <textarea id="message" name="message" rows="5" cols="40"></textarea><br><br>
         <input type="submit" value="Submit">
-</form>
+    </form>
+   
 
-      <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $userInput = $_POST["message"];
-            echo "<p>" . $userInput . "</p>";
-        }
-    ?>
 </body>
-
 </html>
